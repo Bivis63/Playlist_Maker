6df1d145b9fd05b112984c8b596dcd237d70dc23
Bivis <lumenfan63@mail.ru>
2023-02-10 01:29:44 +0400

    убрал FrameLayout и ImageView, оставил только TextView с атрибутом app:drawableStartCompat как посоветовали

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,16 +1,19 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings2)
 
-        val arrowBack = findViewById<ImageView>(R.id.arrowBack)
+        val arrowBack = findViewById<TextView>(R.id.arrowBack)
 
 
         arrowBack.setOnClickListener {
