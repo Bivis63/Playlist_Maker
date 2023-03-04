13b14726e880705c5dd876b77e917ddf648aedd7
Bivis <lumenfan63@mail.ru>
2023-03-05 00:05:20 +0400

    Домашняя работа спринт 10 (классы TrackList и App а разработке :) )

---
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.playlistmaker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.example.playlistmaker.databinding.SongListBinding
+
+class TrackAdapter : RecyclerView.Adapter<TrackAdapter.TrackViewHolder>() {
+
+        var tracksList = ArrayList<Track>()
+
+
+    class TrackViewHolder(item: View) : RecyclerView.ViewHolder(item) {
+        val binding = SongListBinding.bind(item)
+        fun bind(track: Track) = with(binding) {
+            nameTrack.text = track.trackName
+            nameArtist.text = track.artistName
+            timeTrack.text = track.trackTime
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .transform(RoundedCorners(10))
+                .placeholder(R.drawable.place_holder)
+                .into(imageUrl)
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.song_list,parent,false)
+        return TrackViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+       return tracksList.size
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(tracksList[position])
+    }
+    fun addTrack (track: Track){
+        tracksList.add(track)
+        notifyDataSetChanged()
+
+    }
+
+}
