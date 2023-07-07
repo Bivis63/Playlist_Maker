cf8eab9b975ac0432f2b5c46a9059c51cd25a94d
Bivis <lumenfan63@mail.ru>
2023-07-07 08:19:57 +0400

    дз 18 спринт(поправил картинки плейсхолдеров, обработал startActivity через try catch

--- app/src/main/java/com/example/playlistmaker/sharing/data/impl/ExternalNavigatorImpl.kt
+++ app/src/main/java/com/example/playlistmaker/sharing/data/impl/ExternalNavigatorImpl.kt
@@ -10,33 +10,44 @@ class ExternalNavigatorImpl(private val context: Context) : ExternalNavigator {
 
 
     override fun shareLink(url: String) {
-        val sendIntent = Intent(Intent.ACTION_SEND)
-        sendIntent.type = "text/plain"
-        sendIntent.putExtra(Intent.EXTRA_TEXT, url)
-        sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(
-            Intent.createChooser(sendIntent, null).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        )
+        try {
+            val sendIntent = Intent(Intent.ACTION_SEND)
+            sendIntent.type = "text/plain"
+            sendIntent.putExtra(Intent.EXTRA_TEXT, url)
+            sendIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(
+                Intent.createChooser(sendIntent, null).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun openLink(url: String) {
-        val shareIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-        shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        context.startActivity(shareIntent)
+        try {
+            val shareIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            context.startActivity(shareIntent)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     override fun openEmail(
         emailData: EmailData
     ) {
-        val intent = Intent(Intent.ACTION_SENDTO).apply {
-            data = Uri.parse("mailto:")
-            putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
-            putExtra(Intent.EXTRA_TEXT, emailData.message)
-            putExtra(Intent.EXTRA_SUBJECT, emailData.theme)
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        try {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(emailData.email))
+                putExtra(Intent.EXTRA_TEXT, emailData.message)
+                putExtra(Intent.EXTRA_SUBJECT, emailData.theme)
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            }
+            context.startActivity(intent)
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        context.startActivity(intent)
-
     }
 
 }
